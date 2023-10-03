51f6f05561b9ca74529ce1a19417fab66818c10c
NachoJur <nachojur@gmail.com>
2023-10-03 18:33:19 -0300

    Initial commit

---
+++ src/provemax_grupo_5/Entidades/Proveedor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provemax_grupo_5.Entidades;
+
+/**
+ *
+ * @author nacho
+ */
+public class Proveedor {
+    private int idProveedor;
+    
+    
+}
